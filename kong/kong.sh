42f30ad570d3f441b70c58adcd2066fbbdfab926
claudioed <claudioed.oliveira@gmail.com>
2022-09-28 06:51:41 -0300

    configure kong

--- kong/kong.sh
+++ kong/kong.sh
@@ -2,4 +2,5 @@
 helm repo add kong https://charts.konghq.com
 helm repo update
 kubectl create namespace api-gateway
-helm install kong kong/kong  --set ingressController.installCRDs=false --namespace api-gateway
+kubectl label namespace api-gateway istio-injection=enabled --overwrite
+helm install kong kong/kong -f kong-values.yaml --set proxy.type=NodePort,proxy.http.nodePort=30000,proxy.tls.nodePort=30003 --set ingressController.installCRDs=false --namespace api-gateway
