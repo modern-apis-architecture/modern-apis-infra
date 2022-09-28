a03eba1f97accae5e8cd0fd638a673eca7613bff
claudioed <claudioed.oliveira@gmail.com>
2022-09-28 18:54:17 -0300

    configure prometheus

--- keycloak/keycloak.sh
+++ keycloak/keycloak.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kubectl create ns iam
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install keycloak bitnami/keycloak --set auth.adminUser=keycloak,auth.adminPassword=keycloak --namespace iam
+helm install keycloak bitnami/keycloak --set auth.adminUser=keycloak,auth.adminPassword=keycloak --version 10.0.0 --namespace iam
